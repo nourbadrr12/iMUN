cb9057c244666e8c7c7faa60c2e767b91ed57b87
nourbadrr12 <125277180+nourbadrr12@users.noreply.github.com>
2023-06-27 00:03:56 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,327 @@
+// Loop through Array of Objects
+var objPeople = [
+	{ // Object @ 0 index
+		username: "USA",
+		password: "11"
+	},
+	{ // Object @ 1 index
+		username: "USA",
+		password: "22"
+	},
+	{ // Object @ 2 index
+		username: "Egypt",
+		password: "33"
+	},
+	{ username: "UAE",
+	password: "44px"
+},
+{ username: "Rpre",
+password: "55"
+},{ username: "INEa",
+password: "66"
+},{ username: "chh",
+password: "77"
+},{ username: "chh",
+password: "88"
+},{ username: "chh",
+password: "99"
+},{ username: "chh",
+password: "1010"
+},{ username: "chh",
+password: "1111"
+},{ username: "chh",
+password: "1212"
+},{ username: "chh",
+password: "1313"
+},{ username: "chh",
+password: "1414"
+},{ username: "chh",
+password: "15"
+},{ username: "chh",
+password: "16"
+},{ username: "chh",
+password: "17"
+},{ username: "chh",
+password: "18"
+},{ username: "chh",
+password: "19"
+},{ username: "chh",
+password: "20"
+},{ username: "chh",
+password: "21"
+},{ username: "chh",
+password: "22"
+},{ username: "chh",
+password: "23"
+},{ username: "chh",
+password: "24"
+},{ username: "chh",
+password: "25"
+},{ username: "chh",
+password: "26"
+},{ username: "chh",
+password: "27"
+},{ username: "chh",
+password: "28"
+},{ username: "chh",
+password: "29"
+},{ username: "chh",
+password: "30"
+},{ username: "chh",
+password: "31"
+},{ username: "chh",
+password: "32"
+},{ username: "chh",
+password: "33"
+},{ username: "chh",
+password: "34"
+},{ username: "chh",
+password: "35"
+},{ username: "chh",
+password: "36"
+},{ username: "chh",
+password: "37"
+},{ username: "chh",
+password: "38"
+},{ username: "chh",
+password: "39"
+},{ username: "chh",
+password: "40"
+},{ username: "chh",
+password: "41"
+},{ username: "chh",
+password: "42"
+},{ username: "chh",
+password: "43"
+},{ username: "chh",
+password: "44"
+},{ username: "chh",
+password: "45"
+},{ username: "chh",
+password: "46"
+},{ username: "chh",
+password: "47"
+},{ username: "chh",
+password: "48"
+},{ username: "chh",
+password: "49"
+},{ username: "chh",
+password: "50"
+},{ username: "chh",
+password: "51"
+},{ username: "chh",
+password: "52"
+},{ username: "chh",
+password: "53"
+},{ username: "chh",
+password: "54"
+},{ username: "chh",
+password: "55"
+},{ username: "chh",
+password: "56"
+},{ username: "chh",
+password: "57"
+},{ username: "chh",
+password: "58"
+},{ username: "chh",
+password: "59"
+},{ username: "chh",
+password: "60"
+},{ username: "chh",
+password: "61"
+},{ username: "chh",
+password: "62"
+},{ username: "chh",
+password: "63"
+},{ username: "chh",
+password: "64"
+},{ username: "chh",
+password: "65"
+},{ username: "chh",
+password: "66"
+},{ username: "chh",
+password: "67"
+},{ username: "chh",
+password: "68"
+},{ username: "chh",
+password: "69"
+},{ username: "chh",
+password: "70"
+},{ username: "chh",
+password: "71"
+},{ username: "chh",
+password: "72"
+},{ username: "chh",
+password: "73"
+},{ username: "chh",
+password: "74"
+},{ username: "chh",
+password: "75"
+},{ username: "chh",
+password: "76"
+},{ username: "chh",
+password: "77"
+},{ username: "chh",
+password: "78"
+},{ username: "chh",
+password: "79"
+},{ username: "chh",
+password: "80"
+},{ username: "chh",
+password: "81"
+},{ username: "chh",
+password: "82"
+},{ username: "chh",
+password: "83"
+},
+{ username: "chh",
+password: "84"
+},{ username: "chh",
+password: "85"
+},{ username: "chh",
+password: "86"
+},{ username: "chh",
+password: "87"
+},{ username: "chh",
+password: "88"
+},{ username: "chh",
+password: "89"
+},{ username: "chh",
+password: "90"
+},{ username: "chh",
+password: "91"
+},{ username: "chh",
+password: "92"
+},{ username: "chh",
+password: "93"
+},{ username: "chh",
+password: "94"
+},{ username: "chh",
+password: "95"
+},{ username: "chh",
+password: "96"
+},{ username: "chh",
+password: "97"
+},{ username: "chh",
+password: "98"
+},{ username: "chh",
+password: "99"
+},{ username: "chh",
+password: "100"
+},{ username: "chh",
+password: "101"
+},{ username: "chh",
+password: "102"
+},{ username: "chh",
+password: "103"
+},{ username: "chh",
+password: "104"
+},{ username: "chh",
+password: "105"
+},{ username: "chh",
+password: "106"
+},{ username: "chh",
+password: "107"
+},{ username: "chh",
+password: "108"
+},{ username: "chh",
+password: "109"
+},{ username: "chh",
+password: "110"
+},{ username: "chh",
+password: "111"
+},{ username: "chh",
+password: "112"
+},{ username: "chh",
+password: "113"
+},{ username: "chh",
+password: "114"
+},{ username: "chh",
+password: "115"
+},{ username: "chh",
+password: "116"
+},{ username: "chh",
+password: "117"
+},{ username: "chh",
+password: "118"
+},{ username: "chh",
+password: "119"
+},{ username: "chh",
+password: "120"
+},{ username: "chh",
+password: "121"
+},{ username: "chh",
+password: "122"
+},{ username: "chh",
+password: "123"
+},{ username: "chh",
+password: "124"
+},{ username: "chh",
+password: "125"
+},{ username: "chh",
+password: "126"
+},{ username: "chh",
+password: "127"
+},{ username: "chh",
+password: "128"
+},{ username: "chh",
+password: "129"
+},{ username: "chh",
+password: "130"
+},{ username: "chh",
+password: "131"
+},{ username: "chh",
+password: "132"
+},{ username: "chh",
+password: "133"
+},{ username: "chh",
+password: "134"
+},{ username: "chh",
+password: "135"
+},{ username: "chh",
+password: "136"
+},{ username: "chh",
+password: "137"
+},{ username: "chh",
+password: "138"
+},{ username: "chh",
+password: "139"
+},{ username: "chh",
+password: "140"
+},{ username: "chh",
+password: "141"
+},{ username: "chh",
+password: "142"
+},{ username: "chh",
+password: "143"
+},{ username: "chh",
+password: "144"
+},{ username: "chh",
+password: "145"
+},{ username: "chh",
+password: "146"
+},{ username: "chh",
+password: "147"
+},{ username: "chh",
+password: "148"
+},{ username: "chh",
+password: "149"
+},{ username: "chh",
+password: "150"
+}
+]
+
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			window.location="registration.html";
+			// stop the function if this is found to be true
+			return
+		}
+	}
+	alert("incorrect username or password")
+}
