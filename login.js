b566b626d8d306766c16e02fc7ebcf8865f3d048
nourbadrr12 <125277180+nourbadrr12@users.noreply.github.com>
2023-07-14 19:13:27 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,326 @@
+// Loop through Array of Objects
+var objPeople = [
+	{ // Object @ 0 index
+		username: "fg9",
+		password: "11"
+	},
+	{ // Object @ 1 index
+		username: "kj9",
+		password: "22"
+	},
+	{ // Object @ 2 index
+		username: "ki0",
+		password: "33"
+	},
+	{ username: "lm4",
+	password: "44px"
+},
+{ username: "Rpre",
+password: "55"
+},{ username: "INEa",
+password: "61"
+},{ username: "chh",
+password: "77"
+},{ username: "jjk",
+password: "88"
+},{ username: "qwm7",
+password: "99"
+},{ username: "mm00",
+password: "1010"
+},{ username: "ny9",
+password: "19"
+},{ username: "hj2",
+password: "1212"
+},{ username: "apn",
+password: "1313"
+},{ username: "nn0",
+password: "1414"
+},{ username: "mml1",
+password: "15"
+},{ username: "cb3",
+password: "16"
+},{ username: "csc",
+password: "17"
+},{ username: "hwx",
+password: "18"
+},{ username: "vhx",
+password: "19"
+},{ username: "ci2",
+password: "20"
+},{ username: "kkz",
+password: "21"
+},{ username: "zz8",
+password: "22"
+},{ username: "iu8",
+password: "23"
+},{ username: "rt5",
+password: "24"
+},{ username: "we3",
+password: "25"
+},{ username: "qw2",
+password: "26"
+},{ username: "nm0",
+password: "27"
+},{ username: "hj8",
+password: "28"
+},{ username: "dfr4",
+password: "29"
+},{ username: "sde4",
+password: "30"
+},{ username: "qws3",
+password: "31"
+},{ username: "bvg6",
+password: "32"
+},{ username: "df5",
+password: "33"
+},{ username: "kl5",
+password: "34"
+},{ username: "spn6",
+password: "35"
+},{ username: "qih9",
+password: "36"
+},{ username: "avt0",
+password: "37"
+},{ username: "jj2",
+password: "38"
+},{ username: "ae3",
+password: "39"
+},{ username: "ea2",
+password: "40"
+},{ username: "llm",
+password: "41"
+},{ username: "ft6",
+password: "42"
+},{ username: "kii",
+password: "43"
+},{ username: "ll9",
+password: "44"
+},{ username: "gg3",
+password: "45"
+},{ username: "dd2",
+password: "46"
+},{ username: "aqq",
+password: "47"
+},{ username: "ii9",
+password: "48"
+},{ username: "hju",
+password: "49"
+},{ username: "mmb",
+password: "50"
+},{ username: "kjj",
+password: "51"
+},{ username: "ery",
+password: "52"
+},{ username: "nb8",
+password: "53"
+},{ username: "nb0",
+password: "54"
+},{ username: "eryv",
+password: "55"
+},{ username: "jhv",
+password: "56"
+},{ username: "uv0",
+password: "57"
+},{ username: "cti",
+password: "58"
+},{ username: "rx2",
+password: "59"
+},{ username: "w2p",
+password: "60"
+},{ username: "pyf",
+password: "61"
+},{ username: "ytj",
+password: "62"
+},{ username: "s3o",
+password: "63"
+},{ username: "0j9",
+password: "64"
+},{ username: "d9o",
+password: "65"
+},{ username: "hu8",
+password: "66"
+},{ username: "8hm",
+password: "67"
+},{ username: "jij0",
+password: "68"
+},{ username: "sd4",
+password: "69"
+},{ username: "vj8n",
+password: "70"
+},{ username: "dvg6",
+password: "71"
+},{ username: "jki9",
+password: "72"
+},{ username: "fgv7",
+password: "73"
+},{ username: "dcg6",
+password: "74"
+},{ username: "nmi9",
+password: "75"
+},{ username: "fgt6",
+password: "76"
+},{ username: "eno7",
+password: "77"
+},{ username: "e4f",
+password: "78"
+},{ username: "e2q8",
+password: "79"
+},{ username: "hwb",
+password: "80"
+},{ username: "cik",
+password: "81"
+},{ username: "klo0",
+password: "82"
+},{ username: "uu8",
+password: "83"
+},{ username: "rt5",
+password: "84"
+},{ username: "ghy7",
+password: "85"
+},{ username: "nmj9",
+password: "86"
+},{ username: "ghb7",
+password: "87"
+},{ username: "dfr5",
+password: "88"
+},{ username: "df5",
+password: "89"
+},{ username: "hj99",
+password: "90"
+},{ username: "vbg6",
+password: "91"
+},{ username: "nm66",
+password: "92"
+},{ username: "cdf5",
+password: "93"
+},{ username: "sfg4",
+password: "94"
+},{ username: "sno9",
+password: "95"
+},{ username: "ev7",
+password: "96"
+},{ username: "scv6",
+password: "97"
+},{ username: "df0",
+password: "98"
+},{ username: "ghb7",
+password: "99"
+},{ username: "rtgp",
+password: "100"
+},{ username: "bu8b",
+password: "101"
+},{ username: "chhr",
+password: "102"
+},{ username: "ff1t",
+password: "103"
+},{ username: "iub",
+password: "104"
+},{ username: "yvo",
+password: "105"
+},{ username: "uv89",
+password: "106"
+},{ username: "yxr6",
+password: "107"
+},{ username: "ct78",
+password: "108"
+},{ username: "guf8",
+password: "109"
+},{ username: "uvy8",
+password: "110"
+},{ username: "8fj",
+password: "111"
+},{ username: "uv7",
+password: "112"
+},{ username: "ib8",
+password: "113"
+},{ username: "ijb7",
+password: "114"
+},{ username: "uhi",
+password: "115"
+},{ username: "by0",
+password: "116"
+},{ username: "hhu8",
+password: "117"
+},{ username: "cyy7",
+password: "118"
+},{ username: "koim",
+password: "119"
+},{ username: "by6",
+password: "120"
+},{ username: "uv0",
+password: "121"
+},{ username: "ggy4",
+password: "122"
+},{ username: "brj4",
+password: "123"
+},{ username: "eb8",
+password: "124"
+},{ username: "ndje",
+password: "125"
+},{ username: "ejei",
+password: "126"
+},{ username: "eiek",
+password: "127"
+},{ username: "jjwe",
+password: "128"
+},{ username: "djn0",
+password: "129"
+},{ username: "wjej",
+password: "130"
+},{ username: "wjbb",
+password: "131"
+},{ username: "he01",
+password: "132"
+},{ username: "w2sb",
+password: "133"
+},{ username: "hys2",
+password: "134"
+},{ username: "kki0",
+password: "135"
+},{ username: "iji8",
+password: "136"
+},{ username: "uu1",
+password: "137"
+},{ username: "unu1",
+password: "138"
+},{ username: "kw7",
+password: "139"
+},{ username: "hwh7",
+password: "140"
+},{ username: "wtr2",
+password: "141"
+},{ username: "kio9",
+password: "142"
+},{ username: "laj9",
+password: "143"
+},{ username: "qlqi",
+password: "144"
+},{ username: "qjwi0",
+password: "145"
+},{ username: "qkq1",
+password: "146"
+},{ username: "ll1",
+password: "147"
+},{ username: "wiq2",
+password: "148"
+},{ username: "llwq",
+password: "149"
+},{ username: "un2",
+password: "150"
+}
+]
+
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			window.location="registration.html";
+			// stop the function if this is found to be true
+			return
+		}
+	}
+	alert("incorrect username or password")
+}
